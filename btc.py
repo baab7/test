08fdeeb6fb04862733911dfa121ae6ab4937dd23
baab7 <56291032+baab7@users.noreply.github.com>
2021-07-19 17:18:41 +0700

    Update btc.py

--- btc.py
+++ btc.py
@@ -4,11 +4,11 @@ import os
 import sys
 import requests
 import json
-@borg.on(events.NewMessage(pattern=r"\.kursbtc", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.btc", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
        
     r=requests.get('https://blockchain.info/ru/ticker').json()["RUB"]["buy"]
     await event.edit(f"""**BITCOIN**
-1 BTC: {r}₽""")
+1 ```BTC```: {r}₽""")
