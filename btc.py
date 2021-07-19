3b5f4240b15b92c8fa0f92d3293f7b356a8b548c
baab7 <56291032+baab7@users.noreply.github.com>
2021-07-19 17:15:06 +0700

    Create btc.py

---
+++ btc.py
@@ -0,0 +1,14 @@
+from telethon import events
+import asyncio
+import os
+import sys
+import requests
+import json
+@borg.on(events.NewMessage(pattern=r"\.kursbtc", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+       
+    r=requests.get('https://blockchain.info/ru/ticker').json()["RUB"]["buy"]
+    await event.edit(f"""**BITCOIN**
+1 BTC: {r}₽""")
